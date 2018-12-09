252bd3604d4051bf394aafd9e886770ec8872c19
sepanther <sepanta.mohseni@gmail.com>
2018-12-08 20:14:00 -0500

    fixed some bugs

--- liri.js
+++ liri.js
@@ -11,7 +11,6 @@ var spotify = new Spotify({
 var fs = require("fs")
 
 
-// console.log(keys);
 
 var command = process.argv[2]
 var workOfArt = process.argv.slice(3).join(" ");
@@ -23,9 +22,7 @@ function whatToDo() {
                 console.log("I don't know what it says!")
             }
             command = data.substring(0, data.indexOf(","))
-            workOfArt = data.substring(data.indexOf(",") + 1, data.length - 1)
-            console.log("Command: " + command)
-            console.log("Work of art: " + workOfArt)
+            workOfArt = data.substring(data.indexOf(",") + 2, data.length - 1)
             whatToDo();
         })
     }
@@ -52,33 +49,43 @@ function whatToDo() {
 
 //Function for concert-this
 function ConcertThis() {
-    // var workOfArt = process.argv.slice(3).join(" ")
-    axios.get("https://rest.bandsintown.com/artists/" + workOfArt + "/events?app_id=codingbootcamp")
-    .then(function(response) {
-        var results = response.data;
-        for (i=0;i<results.length;i++) {
-            var venue = results[i].venue.name;
-            if (results[i].country === "United States") {
-                var location = results[i].venue.city + ", " + results[i].venue.region
+    if (workOfArt == "") {
+        console.log("You must include an artist to search.")
+    }
+    else {
+        axios.get("https://rest.bandsintown.com/artists/" + workOfArt + "/events?app_id=codingbootcamp")
+        .then(function(response) {
+            var results = response.data;
+            for (i=0;i<results.length;i++) {
+                var venue = results[i].venue.name;
+                if (results[i].country === "United States") {
+                    var location = results[i].venue.city + ", " + results[i].venue.region
+                }
+                else {
+                    var location = results[i].venue.city + ", " + results[i].venue.country
+                }
+                var date = moment(results[i].datetime)
+                date = date.format("MM/DD/YYYY")
+                var output = ("\nVenue: " + venue + "\nLocation: " + location + "\nDate: " + date + "\n---------------------------------");
+                console.log(output)
+                fs.appendFile('log.txt', output, 'utf8', function(error) {
+                    if (error) {
+                        console.log("Oops! Couldn't write.")
+                    }
+                    console.log("Yay! Appended data to file.")
+                })
             }
-            else {
-                var location = results[i].venue.city + ", " + results[i].venue.country
-            }
-            var date = moment(results[i].datetime)
-            date = date.format("MM/DD/YYYY")
-            console.log("Venue: " + venue + "\nLocation: " + location + "\nDate: " + date + "\n---------------------------------");
-        }
-    })
+        })
+    }
+
 }
 
 //Function for 'spotify-this-song
 function SpotifyThis() {
-    // var workOfArt = process.argv.slice(3).join(" ");
     console.log("SpotifyThis says workOfArt is: ")
     if (workOfArt == "") {
         workOfArt = "The Sign Ace of Base"
     }
-    // console.log(song)
     spotify.search({
         type: 'track',
         query: workOfArt
@@ -94,25 +101,17 @@ function SpotifyThis() {
         var output = ("\nArtist: " + artist + "\nSong Name: " + name + "\nPreview Link: " + preview + "\nAlbum: " + album + "\n---------------------------------");
         console.log(output)
         fs.appendFile('log.txt', output, 'utf8', function(error) {
-            if (err) {
+            if (error) {
                 console.log("Oops! Couldn't write.")
             }
             console.log("Yay! Appended data to file.")
         })
-        // for (i=0; i<results.length; i++) {
-        //     var artist = results[i].artists[0].name;
-        //     var name = results[i].name;
-        //     var preview = results[i].preview_url;
-        //     var album = results[i].album.name;
-        //     console.log("Artist: " + artist + "\nSong Name: " + name + "\nPreview Link: " + preview + "\nAlbum: " + album + "\n---------------------------------");
-        // }
     })
 }
 
 
 //Function for movie-this
 function MovieThis() {
-    // var workOfArt = process.argv.slice(3).join(" ");
     if (workOfArt === "") {
         workOfArt = "Mr. Nobody"
     }
@@ -139,13 +138,15 @@ function MovieThis() {
         language = results.Language;
         plot = results.Plot;
         actors = results.Actors;
-        console.log("Title: " + title + "\nYear: " + year + "\nIMDB Rating: " + IMDB + "\nRotten Tomatoes Rating: " + RT + "\nCountry: " + country + "\nLanguage: " + language + "\nPlot: " + plot + "\nActors: " + actors + "\n---------------------------------")
+        var output = ("\nTitle: " + title + "\nYear: " + year + "\nIMDB Rating: " + IMDB + "\nRotten Tomatoes Rating: " + RT + "\nCountry: " + country + "\nLanguage: " + language + "\nPlot: " + plot + "\nActors: " + actors + "\n---------------------------------")
+        console.log(output)
+        fs.appendFile('log.txt', output, 'utf8', function(error) {
+            if (error) {
+                console.log("Oops! Couldn't write.")
+            }
+            console.log("Yay! Appended data to file.")
+        })
     })
-}
-
-//Function to write to log.txt
-function writeToFile() {
-    
 }
 
 whatToDo();
